bec488f7bd760475b3ae60ccc3d9aa4be00973e8
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-01-31 16:23:45 +0000

    Submitted Work

---
+++ springintrotopython/kickoffwarmup.py
@@ -0,0 +1,5 @@
+# In your own words, what do you think a computer program is and what they do?
+print("I believe that a computer program is a combination of similar and different programming languages and they work together to formulate a software that is interactive.")
+
+#What is your favorite food?
+print("My favorite food is Chicken and Shrimp Carbonara")
