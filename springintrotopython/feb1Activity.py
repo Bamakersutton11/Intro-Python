11fb4cb5d9b153448a4ab9cee3edef050128fba3
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-02-01 16:20:29 +0000

    Finished Work

---
+++ springintrotopython/feb1Activity.py
@@ -0,0 +1,16 @@
+# A variable is something that has no defined value and can be changed to fit a certain piece. 
+# A difference between an integer and a float is that the integer uses whole numbers, while the float isn't limited to that.
+# Boolean data types can only have two states: true or false (ex: 5 > 3)
+
+myName = "Braheem Amaker - Sutton"
+GPA = 4.0
+age = 17
+BL = "Student at Boys Latin"
+
+print(f"Your name is {myName}")
+print(f"You are {age} years old")
+print(f"You're a {BL}")
+print(f"Your gpa is {GPA}")
+
+# A time where techonolgy helped me in an unexpected way is when I didn't know how to get home, then I remembered that I could use a google gps to find which bus I needed to take.
+# Losing access to technology would not just be bad because the loss of entertainment, but also because there are necessities that can be lost as well. Like not being able to work, do homework, pay bills, and etc.
