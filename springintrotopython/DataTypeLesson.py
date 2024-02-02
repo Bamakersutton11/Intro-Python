5e219e3c7ffe65a238c808ce8fa740d0c46da67a
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-02-02 16:04:07 +0000

    2/2/24 Classwork

---
+++ springintrotopython/DataTypeLesson.py
@@ -0,0 +1,21 @@
+#Data Types
+
+# input takes in data from a user.
+# input()
+
+name = input('Please enter your name:')
+
+# print outputs data from your program.
+# print()
+
+print(f'Hello, {name}.' + ' I am Goku.')
+
+age = input('How old are you?: ')
+
+print('You are ' + age, 'years old, cool. I am a few hundred years old.')
+
+favoriteFood = input('What is your favorite food?: ')
+
+print('I love ' + favoriteFood, 'too!!')
+
+# variables are placeholders/containers for data (datatypes)
