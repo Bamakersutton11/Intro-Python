27dedf048f8112d015f0841d076bd2c0a9252de7
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-02-06 16:19:26 +0000

    submitted work 2/6/24

---
+++ springintrotopython/Feb6activity.py
@@ -0,0 +1,38 @@
+# Q: What is complexity?
+# A: The measure of how intensive or
+# resourceful a program can be.
+
+# Q: What is abstraction?
+# A: Abstraction is the process of
+# simplifying or hiding complexity.
+
+#Camel Case is in code where every word after another
+#is capitalized as seen below
+
+teeShirtSizeSm = False
+teeShirtSizeMd = False
+teeShirtSizeLg = True
+
+#SnakeCase = Every new word after the first gets an underscore
+characterObject_Movement = [3903]
+
+#PascalCase = Every word is capitalized,
+# including the first word.
+GuestInMath = ''
+GuestInLiterature = ''
+
+#Arithmetic Actions
+varOne = int(input('Enter number: '))
+varTwo = int(input('Enter number: '))
+print(varOne + varTwo)
+
+#Assignment Actions
+
+x = 10
+x -= 3
+print(x)
+
+#Comparison Actions
+orderOfApples = 10
+orderOfOranges = 11
+print(orderOfApples < orderOfOranges)
