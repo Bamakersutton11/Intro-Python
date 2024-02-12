263f55fa8680c39425e64334d8c48966b2a3c870
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-02-12 15:57:56 +0000

    Work for 2/12/24

---
+++ springintrotopython/Feb12activity.py
@@ -0,0 +1,8 @@
+#3 What does TypeError mean?
+# The data type used was mot the data type the computer expected.
+
+print('2' + 2)
+
+#4 What does SyntaxError mean?
+
+print('Braheem)
---
+++ springintrotopython/warmupFeb12.py
@@ -0,0 +1,12 @@
+#The data type print(str(20)) will print a string data type because the parentheses because of the str function.
+
+# The data type print(20.901) will print is a float because it has a decimal.
+
+#When you encounter a TypeError, most likely you have combined the wrong data types.
+
+#When you encounter a SyntaxError, most likely you've has spelled a code instruction  wrong.
+
+#Python list are ordered, changeable, and duplicatable
+
+GroceryList: print['books', 'apple']
+
