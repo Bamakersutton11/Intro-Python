a446ef3f04e2e66c67fe5d4878fd92909acce140
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-02-14 16:24:28 +0000

    Work for 2/14/24

---
+++ springintrotopython/warmupFeb14.py
@@ -0,0 +1,22 @@
+# 1) A built-in function is a function that the computer already
+# recognizes and can be used by a programmer to setup instructions 
+# for the computer to read.
+
+# 2) One built-in function that we've used in class are the 
+# print() and list functions.
+
+# 3) Some datatypes can be assigned to a python list. These include
+# things like: integers, strings, booleans, and etc.
+
+groceryList = []
+amazonList = ['Week', 25, 2.32, True]
+print(amazonList)
+
+# def = defintion of a function. To use the function, you should write the name of he function. (Ex. )
+
+def grilledCheese():
+    print('Turn stove on to 350 degrees to heat up pan')
+    print('Apply butter to the pan until melted')
+    print('Add the cheese to the bread in a sandwich form')
+    print('Place sandwich pan and cook evenly on both sides until golden brown')
+
