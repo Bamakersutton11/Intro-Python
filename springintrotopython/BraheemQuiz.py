6c215183ba8c87bdc4a79871cd0ca768bd0e5af4
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-02-16 16:04:07 +0000

    Quiz for 2.16.24

---
+++ springintrotopython/BraheemQuiz.py
@@ -0,0 +1,86 @@
+#1. What built-in function would you use if you wanted
+# to print out data to a user? The built-in function I would use is the
+# print() function.
+
+#2. What built-in function would you use if you wanted 
+# a user to pass in data to a program? I would use the input() function.
+
+#3. Explain what complexity and abstraction are, then 
+# provide an example describing their relationship to one another.
+# You cannot use the following examples because they 
+# were shared in class; grocery/ electronics store, car, phone. 
+
+# Complexity is understanding the process of an outcome,
+# like knowing what ingredients to make a game. While
+# abstraction is the way of simplifying those steps.
+
+#4. What does it mean when you encounter a syntax error? 
+# When you encounter a syntax error, it's telling you that you
+# wrote something that the computer doesn't understand. This is usually
+# when you write the a function the wrong way.
+
+#5. What is a variable? Provide an example of a variable in code.
+# A variable is a container for any unknown data that can vary. 
+k = 'Kite' 
+
+#6. Write a function in Python that when called will print out 
+# your name.
+
+b = 'Braheem'
+print(b)
+
+#7. What is a function definition and a function call?
+# A function definition allows the the computer to store instructions 
+# and, a function call allows the instructions to be put in action.
+
+#8. Describe what each symbol means?
+
+# = // single equal --> Allows for variables to connect to their value
+# == // double equal --> Shows that two integers are equal
+# != // exclaimation equal --> 
+
+#9. What will the output for the following code block be?
+var1 = 1
+var2 = 2
+var3 = "3"
+
+print(var1 + var2) # --> The output will give you a 
+# TypeError because a string cannot be added with an integer.
+
+
+#10. What will the output for the following code block be?
+listOne = [20, 40, 60, 80]
+listTwo = [20, 40, 60, 80]
+
+print(listOne == listTwo)
+# The output will come out as true.
+
+# 11. Create the following python lists and include the following
+# data types for each list:
+# a- create a python list of strings. The strings should be items you
+# can find in a book bag.
+# b- Create a python list of floats. the floats should represent the 
+# price of each item in a book bag from the previous list.
+stringList = ['notebook', 'pencil', 'eraser']
+floatList = [6.14, 4.82, 5.96]
+
+# 12. What are the Three (3) naming formats we use for naming variables, files, and functions?
+# Camel Case, Pascal Case, and Snake Case 
+
+# 13. Provide a coding scenario where you would you use a comparison operator in a program? 
+# A scenario is when you are making a fighting game where a player gets points to win
+# and making a system where the scores are compared to determine the winner.
+
+# 14. Provide a coding scenario where you would you use a logical operator in a program? 
+# A scenario where I'd use a logical operator in a program is when I
+
+# 15. Create a block of code where you would 
+# use a comparison operator with the follwing data:
+currentScore= 100
+highScore=2300
+
+print(currentScore < highScore)
+
+# Bonus : What does the type() function do?
+# The type() function allows the programmer to determine which
+# datatype is being used for a function.
