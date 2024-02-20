b1f9d759b4b80eb737ddaf5fafb56a79eb9aab1e
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-02-20 16:24:18 +0000

    Work for 2.20.24

---
+++ springintrotopython/warmupFeb20.py
@@ -0,0 +1,36 @@
+laundryList = ['towel', 'rag', 'clothes', 'laundry basket', 'detergent']
+
+print(laundryList[3])
+
+# Notes 
+# 1. Take in user name --> input()
+# 2. Reverse and print the user name --> 
+
+def reverseName():
+    name =  input('What is your name: ')[::-1]
+    print(name)
+
+reverseName()
+
+def cheeseCake():
+    print("Here's how to make a cheesecake...")
+
+def securityLogin():
+    Pin = input('Please enter your pin: ')
+    return Pin
+securityLogin()
+
+def companyLogin(name):
+    print(f'hello {name}')
+
+companyLogin('Braheem')
+
+def math(number):
+    result = number + 10
+    print(result)
+
+math(11)
+
+debit = 10000
+money = input()
+print(debit - money)
