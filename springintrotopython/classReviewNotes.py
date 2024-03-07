b67343f7da2c7cc568489299f55ac25a8e5fe7b9
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-03-07 16:21:48 +0000

    Work for 3/7/24

---
+++ springintrotopython/classReviewNotes.py
@@ -0,0 +1,34 @@
+# PYTHON CLASS REVIEW
+
+# Programs = Instructions for coomputers.
+
+# Programming languages = Different ways we can communicate with computers
+# and give instructions to.
+
+# What is a computer program?
+
+# A set of instructions that a computer can execute in order to perform a
+# specific task or achieve a particular goal.
+
+# Syntax = the formatting rules of a language 
+
+# Note - when we are writing programs, all we are doing is talking to a computer
+# and giving it instructions to complete a task. 
+
+# string "str" = Allows for isolated characters like words
+# integer "int" = A whole number; a non-decimal
+# float = a decimal number
+# Boolean = a true or false value 
+# print "print()" = a function that prints the information you provide
+# input "input()" = a function that takes in the information you provide
+
+# How to write a function in python
+
+def myFunction():
+    print("Here are my instructions...")
+
+# How to write a function in JavaScript
+
+function myFunction(){
+    console.log("Here are my instructions...")
+}
