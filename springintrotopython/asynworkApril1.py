05ea8e528b3bfb22c7c71b22f649467be02100ae
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-04-02 13:58:30 +0000

    Work 4/2/24

---
+++ springintrotopython/asynworkApril1.py
@@ -0,0 +1,34 @@
+# Async Activity - Monday April 1st, 2024.
+
+# Watch the following videos and answer the following questions. 
+# If necessary, research the questions so that you are able to answer them more effictively.
+
+# When you have completed this assignment, save your work as asyncApril1.py.
+# you must submit your work by 11:59 pm today (April 1, 2024) to recieve credit.  
+
+# Video # 1 - Racial wealth gap
+# https://www.youtube.com/watch?v=Mqrhn8khGLM&t=1s
+
+# Video # 2 - Red lining in America 
+# https://www.youtube.com/watch?v=O5FBJyqfoLM
+
+
+# Questions
+
+#1. Search and find the latest (most recent) household income data 
+# for black households compared to white households and write down what you have found.
+# What is the wealth disparity between the two groups? How does this make you feel?
+
+"The latest household income data for black households" 
+"compared to white house income data is vastly incomparable. This makes me feel bad as I believe we as a community are capable of making that change."
+
+#2.  Research and define in your own words what red lining is?
+
+"Red lining is refusing a bank loan because they come from a poor neighborhood."
+
+#3.  Research and define in your own words what compound interest is?
+
+"Compound interest is money that builds on top of more money over time."
+
+
+ 
