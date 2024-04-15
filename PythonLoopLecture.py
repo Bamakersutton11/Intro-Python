4becbd972233e270d52766f00c5a053ebfda5f1a
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-04-15 14:58:15 +0000

    Work

---
+++ PythonLoopLecture.py
@@ -0,0 +1,20 @@
+"1. The difference between a for loop and a while loop is that the for loop is limited and able to be more controlled." 
+"Meanwhile, the while loop repeats infinitely as long as a statement is true and is harder to control."
+
+"2. For the 'while loop' I would use this for things like an online shop." 
+"As I can set it to match inventory and then have it modify whenever there is a purchase."
+"For the 'for loop' I can use this for a game like "
+
+def alarmClock():
+    dayOfWeek = input("What day is it? ")
+    militaryTime = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+    while day == 'sat' and 'sun':
+        for hour in militaryTime:
+            if hour == 5:
+                print(hour)    
+            print("Sound the alarm.")
+    if dayOfWeek == 'sat' or 'sun':
+        print("There is no alarm.")
+
+alarmClock()
+
