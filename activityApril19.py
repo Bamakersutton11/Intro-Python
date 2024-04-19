b9b51b017117b313e458a345f00b062f684f48ed
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-04-19 15:24:35 +0000

    April19work

---
+++ activityApril19.py
@@ -0,0 +1,41 @@
+#1A. Visit the web page link below and read the following article on asking questions:
+# https://www.betterup.com/blog/how-to-ask-good-questions
+
+# 1B. Based on what you've read, in your own words, describe what a good
+# question is.
+
+"A good question is a question asked with some resource already done from"
+"the asker and is asking the person being asked for confirmation or a critism that is given by then one answering it. "
+
+# 1C. In your opinion and in your own words, why do you think its
+# important to be able to ask good questions.
+
+"It is important to ask good questions so that it can help the answerer find a solution or even make sure you know what you need to know."
+
+# 2A. Visit the link web page below and complete the following learning style survey:
+# http://www.educationplanner.org/students/self-assessments/learning-styles-quiz.shtml
+
+
+#2B. Based on your completed survey results, which learning style are you? List, three (3)
+# things from the survey results that you feel best describe your learning style. 
+
+"My learning style is a tactile learner. The traits of my learning style are: hands-on, putting things together , and tinker when bored."
+
+#2B. Based on your completed survey results, do you feel as though the result are arcurate in
+# reflecting your learning style? if yes, explain why, if no, explain why. Please write your
+# response in atleast three (3) complete sentences. 
+
+"I feel that the survey has turned out to be very accurate. The reason is because it"
+"said that I like to tinker when bored(which is true) and I like to have a hands-on experience with all of my work. "
+
+# 3A. Visit the  webp age link below and read the article on how to study:
+# https://summer.harvard.edu/blog/top-10-study-tips-to-study-like-a-harvard-student/
+
+# 3B. Which of the following tips do you feel you need to work on when studying? Explain
+# why in at least three (3) complete sentences.
+"I feel that I need to work on what things I'm weak in and how can I drastically improve in those areas."
+# 3C. Based on your learning style results, and the arcticle you just read on study tips,
+# think about how you can apply the study tips to your learning style. Then, in complete sentences
+# write down four (4) steps you could take to effectively study that is specific you you. 
+
+"I can apply mt learning style by remainig engaged in a few activivies. I can also try just reviewing w3schools to futher my understanding."
