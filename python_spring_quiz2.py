db33f93f36c10f00d6a57eed03eb9090f991cc0b
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-04-26 15:24:18 +0000

    Work 4/26/24

---
+++ python_spring_quiz2.py
@@ -0,0 +1,17 @@
+# What is a computer program?
+"A set of instructions for a computer to follow."
+
+# What is Syntax?
+"Grammar rules for a programming language."
+
+# What is datacasting?
+"Data casting is the process of changing a data type."
+
+# What are operators? 
+""
+
+action_4 = 2 == 2
+# This is going to be true, because the number 2 is the same as the other number 2. 
+
+# What is a list?
+"A list is a collection of data"
--- springintrotopython/gitfundamentals.py
+++ springintrotopython/gitfundamentals.py
@@ -1,1 +1,11 @@
-print('Braheem Amaker - Sutton')
+import datetime
+
+def date_time():
+    x = datetime.datetime.now()
+    AM_PM = (x.strftime("%p"))
+    DoY = (x.strftime("%j"))
+    HOUR = (x.strftime("%I"))
+    MIN = (x.strftime("%M"))
+    SEC = (x.strftime("%S"))
+    print(HOUR,MIN,SEC,AM_PM)
+date_time()
