4414310a77ff33f02353975d9be395cd0757cdec
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-05-13 14:58:12 +0000

    Project

---
+++ Project/Project.py
@@ -0,0 +1,10 @@
+def sports_basketball():
+    chatQuery = input("Hello, my name is Discovery AI. What can I help you with?> ")
+    if chatQuery == "Games" or "Sports":
+        print("Which sport should I look for?")
+        chatQuery2 = input("NBA, NFL, NHL, WNBA, College, or MLB?> ")
+        if chatQuery2 == "NBA":
+            print("here")
+sports_basketball()
+
+
