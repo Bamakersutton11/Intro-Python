e20c85f57ac416615b44a8d0192e13f805f7d8cd
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-05-17 15:24:23 +0000

    Save 2

--- Project/Project.py
+++ Project/Project.py
@@ -1,23 +1,43 @@
 from googlesearch import search
 import webbrowser
+import math
+def DiscoveryAI():
+    def google_search():
+        query = input("Search: ")
+        results = search(query,num=1,start=1,stop=1)
+        for result in results:
+            print(result)
+        return webbrowser.open(result)
+    google_search() 
 
-def sports_query():
-    chatQuery = input("Hello, my name is Discovery AI. What can I help you with?> ")
-    if chatQuery == "Games" or "Sports":
-        print("Which sport should I look for?")
-        chatQuery2 = input("NBA, NFL, NHL, WNBA, College, or MLB?> ")
-        if chatQuery2 == "NBA" or "nba":
-            input("What game would you like to review?> ")
-        elif chatQuery2 == "NFL" or "nfl":
-            input
-#sports_query()
+    def stat_dict():
+        import urllib.request
+        with urllib.request.urlopen() as response:
+            html = [response.read()]
+        print(html)
+        nba_stats = {
+        }
+        print(nba_stats)  
+    stat_dict()
 
-def webSearch():
-    ip=input("What would you like to search for? ")
-    for url in search(ip, stop=1):
-        print(url + "/Algebra")
-        webbrowser.open(ip)
-webSearch()
 
+    def sports_query():
+        chatQuery = input("Hello, my name is Discovery AI. What can I help you with?> ")
+        if chatQuery == "Games" or "Sports":
+            
+            chatQuery2 = input("NBA, NFL, NHL, WNBA, College, or MLB?> ")
+            if chatQuery2 == "NBA" or "nba":
+                input("What game would you like to review?> ")
+            elif chatQuery2 == "NFL" or "nfl":
+                input
+    # sports_query()
+
+    def mathHelp():
+        ip=input("Enter math equation: ")
+        for url in search(ip, stop=1):
+            print(url)
+            webbrowser.open(ip)
+    # mathHelp()
+DiscoveryAI()
 
 
