efbf1178f82791a557a2be93ab190cf355c76d45
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-05-17 12:46:35 +0000

    Save 1

--- Project/Project.py
+++ Project/Project.py
@@ -1,10 +1,23 @@
-def sports_basketball():
+from googlesearch import search
+import webbrowser
+
+def sports_query():
     chatQuery = input("Hello, my name is Discovery AI. What can I help you with?> ")
     if chatQuery == "Games" or "Sports":
         print("Which sport should I look for?")
         chatQuery2 = input("NBA, NFL, NHL, WNBA, College, or MLB?> ")
-        if chatQuery2 == "NBA":
-            print("here")
-sports_basketball()
+        if chatQuery2 == "NBA" or "nba":
+            input("What game would you like to review?> ")
+        elif chatQuery2 == "NFL" or "nfl":
+            input
+#sports_query()
+
+def webSearch():
+    ip=input("What would you like to search for? ")
+    for url in search(ip, stop=1):
+        print(url + "/Algebra")
+        webbrowser.open(ip)
+webSearch()
+
 
 
