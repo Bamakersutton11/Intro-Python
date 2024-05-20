84b6fe373bb759772cc44b741431051d0a44c47c
Braheem Sutton <148572997+Bamakersutton11@users.noreply.github.com>
2024-05-20 04:12:57 +0000

    Save

--- Project/Project.py
+++ Project/Project.py
@@ -8,7 +8,7 @@ def DiscoveryAI():
         for result in results:
             print(result)
         return webbrowser.open(result)
-    google_search() 
+    #google_search() 
 
     def stat_dict():
         import urllib.request
@@ -18,26 +18,14 @@ def DiscoveryAI():
         nba_stats = {
         }
         print(nba_stats)  
-    stat_dict()
+    #stat_dict()
 
-
-    def sports_query():
-        chatQuery = input("Hello, my name is Discovery AI. What can I help you with?> ")
-        if chatQuery == "Games" or "Sports":
-            
-            chatQuery2 = input("NBA, NFL, NHL, WNBA, College, or MLB?> ")
-            if chatQuery2 == "NBA" or "nba":
-                input("What game would you like to review?> ")
-            elif chatQuery2 == "NFL" or "nfl":
-                input
-    # sports_query()
 
     def mathHelp():
-        ip=input("Enter math equation: ")
-        for url in search(ip, stop=1):
-            print(url)
-            webbrowser.open(ip)
-    # mathHelp()
+        userEquation = input("Enter equation> ")
+        userEquation.find("x")
+        print(userEquation.find("x"))
+    mathHelp()
 DiscoveryAI()
 
 
